194637606279d42fc43b09d57456a5412ebf7abd
EgeYTC <egemel2000gam@gmail.com>
2026-02-25 16:15:07 +0300

    Second Practice Update

---
+++ 02_CombatCalculator/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _02_CombatCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int baseDamage = 35;
+            Console.WriteLine("Enter a combo multiplier: ");
+            string userInput = Console.ReadLine();
+            int damage = (int)(double.Parse(userInput)*baseDamage);
+            Console.WriteLine($"HIT! {damage}");
+            
+        }
+    }
+}
