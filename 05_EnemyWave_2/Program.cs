e0123d5a3e09af777a458a2175b7b4ec671b287c
EgeYTC <egemel2000gam@gmail.com>
2026-03-04 16:24:15 +0300

    Practice 5

---
+++ 05_EnemyWave_2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _05_EnemyWave_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentWave = 3;
+            int enemiesToSpawn = currentWave * 5;
+
+            for (int i = 0; i < enemiesToSpawn; i++)
+            {
+                int enemyNumber = i + 1;
+                Console.WriteLine("Spawning Enemy #" + enemyNumber);
+
+            }
+
+
+            Console.WriteLine("All enemies spawned. Good luck!");
+
+
+        }
+
+
+    }
+}
