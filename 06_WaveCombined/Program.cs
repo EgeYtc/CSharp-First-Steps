a4988907269133012f536f718912376b88c6f573
EgeYTC <egemel2000gam@gmail.com>
2026-03-04 17:18:18 +0300

    practice 6

---
+++ 06_WaveCombined/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _06_WaveCombined
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int baseHeatlh = 10;
+            int waveNumber = 1;
+
+            while (baseHeatlh > 0)
+            {
+                Console.WriteLine($"Current Wave: {waveNumber}");
+
+                for (int i = 0; i < 3; i++)
+                {
+                    if (baseHeatlh <= 0)
+                    {
+                        // 2. The Emergency Eject!
+                        break;
+                    }
+
+                    Console.WriteLine("Enemy Spawned!");
+                    baseHeatlh--;
+                }
+
+                waveNumber++;
+
+            }
+
+            Console.WriteLine("Base Destroyed! Game Over");
+        }
+    }
+}
