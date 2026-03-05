f6eb615e154bd5db28de3bb43bbe488f8ab39bf4
EgeYTC <egemel2000gam@gmail.com>
2026-03-05 17:02:29 +0300

    8th methods

---
+++ 08_Methods/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _08_Methods
+{
+    internal class Program
+    {
+        static int TakeDamage(int currentHealth)
+        {
+            Console.WriteLine("Ouch! Took 10damage!");
+
+            int updatedHealth = currentHealth - 10;
+
+            return updatedHealth;   
+           
+        }
+        static void Main(string[] args)
+        {
+            int playerHealth = 100; 
+
+            playerHealth = TakeDamage(playerHealth);
+            Console.WriteLine($"Health after attack 1: {playerHealth}");
+
+            playerHealth = TakeDamage(playerHealth);
+            Console.WriteLine($"Health after attack 1: {playerHealth}");
+
+
+        }
+    }
+}
