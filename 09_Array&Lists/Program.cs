7829a42262d283166c70a12ad3963301492865eb
EgeYTC <egemel2000gam@gmail.com>
2026-03-05 23:23:57 +0300

    9th

---
+++ 09_Array&Lists/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _09_Array_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           List<string> inventory = new List<string>();
+
+            inventory.Add("Sword");
+            inventory.Add("Silver Coin");
+            inventory.Add("Mana Potion");
+
+            Console.WriteLine($"Second item is: {inventory[1]}");
+
+
+            foreach ( string item in inventory ) 
+            {
+                Console.WriteLine(item);    
+            }
+        }
+    }
+}
