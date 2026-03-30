464e82995a32f95eaf50a761f96cd12925e447ea
EgeYTC <egemel2000gam@gmail.com>
2026-03-30 09:55:46 +0300

    Switch

---
+++ 18_Switch/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _18_Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string enemyState = "Attack";
+            
+
+
+            switch (enemyState)
+            {
+                case "Patrol":
+                    Console.WriteLine("The guard is pacing back and forth.");
+                    break;
+                case "Chase":
+                    Console.WriteLine("The guard spots you and runs toward you!");
+                    break;
+                case "Attack":
+                    Console.WriteLine("The guard swings his broadsword!");
+                    break;
+                default:
+                    Console.WriteLine("The guard is standing still, confused.");
+                    break;
+            }
+            }
+    }
+}
