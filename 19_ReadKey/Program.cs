997962dbb0555c63e4719c5e631c1d955de26aa7
EgeYTC <egemel2000gam@gmail.com>
2026-03-30 13:05:27 +0300

    ReadKey

---
+++ 19_ReadKey/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _19_ReadKey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo keyInfo;
+            Console.TreatControlCAsInput = true;
+
+
+            do
+            {
+                keyInfo = Console.ReadKey();
+
+                if ((keyInfo.Modifiers & ConsoleModifiers.Alt) != 0)
+                    Console.Write("ALT+");
+
+                if ((keyInfo.Modifiers & ConsoleModifiers.Shift) != 0)
+                    Console.Write("SHIFT+");
+
+                if (((keyInfo.Modifiers & ConsoleModifiers.Control) != 0))
+                    Console.Write("CTRL+");
+
+
+            } while (keyInfo.Key != ConsoleKey.Escape);
+        }
+    }
+}
